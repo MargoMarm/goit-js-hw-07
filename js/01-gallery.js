f42915075109a0573a18bfe75a1c193b56eb2e4c
MargoMarm <margogerasimenko@gmail.com>
2023-03-19 14:41:56 +0100

    add prettier

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 const galletyRef = document.querySelector(".gallery");
 
-const makeImages = (gallery) => gallery.map(({ preview, original, description }) =>
-	`<li class="gallery__item">
+const makeImages = (gallery) =>
+  gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -16,36 +19,29 @@ const makeImages = (gallery) => gallery.map(({ preview, original, description })
     />
   </a>
 </li>`
-).join('');
-	
+    )
+    .join("");
+
 const images = makeImages(galleryItems);
 
 galletyRef.insertAdjacentHTML("afterbegin", images);
 
 galletyRef.addEventListener("click", onImageClick);
 
-
 function onImageClick(event) {
-	
-	event.preventDefault();
+  event.preventDefault();
 
-	if (event.target.nodeName !== "IMG") {
-		return
-	}
-		const instance = basicLightbox.create(`
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600">
 `);
-	instance.show()
-	
+  instance.show();
 
-	galletyRef.addEventListener('keydown', (event) => {
-		if (event.code === "Escape") {
-			instance.close()
-
-	}
-})
+  galletyRef.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
 }
-
-
-
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 const galletyRef = document.querySelector(".gallery");
 
-const makeImages = (gallery) => gallery.map(({ preview, original, description }) =>
-	`<li class="gallery__item">
+const makeImages = (gallery) =>
+  gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}"/>
    </a>
 </li>`
-).join('');
-	
+    )
+    .join("");
+
 const images = makeImages(galleryItems);
 galletyRef.insertAdjacentHTML("afterbegin", images);
 
-
-const lightbox = new SimpleLightbox('.gallery a', {captionsData : "alt",captionDelay: 250 });
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
